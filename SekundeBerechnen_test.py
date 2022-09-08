eb723352a7f83ae9252170a4d05956ecb867be3f
Marco Savoca <109957478+masa-gymmich@users.noreply.github.com>
2022-09-08 21:24:49 +0200

    Add files via upload

---
+++ SekundeBerechnen_test.py
@@ -0,0 +1,9 @@
+import SekundenBerechnen
+import builtins
+
+def test_SekundeBerechnen(monkeypatch):
+    inputs = iter(['7', '21', '37'])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+    result = SekundenBerechnen.sekundenBerechnen()
+    print(result)
+    assert result == '7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden.' or result == '7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden'
---
+++ SekundenBerechnen.py
@@ -0,0 +1,38 @@
+# Berechne die Gesamtanzahl von Sekunden anhand gegebener Stunden, Minuten und Sekunden.
+
+# Die nächste Zeile nicht verändern!
+def sekundenBerechnen():
+    
+###################################################
+# Frage hier die Stunden, die Minuten sowie die Sekunden ab und wandle den string in einen integer um. Weise den Wert jeweils einer Variable zu. Zwei Variablen fehlen noch!!
+    stunden = int(input("Stunden?"))
+    minuten = int(input("Minuten?"))
+    sekunden = int(input("Sekunden?"))
+
+
+
+###################################################
+# Formel zur Umwandlung con Stunden, Minuten, Sekunden in Sekunden
+# Du sollttest deinen code unter dieser Zeile einfügen. Das Ergebnis der Rechnung wird der Variable total_sekunden zugewiesen.
+
+    total_sekunden = 3600*stunden + 60*minuten + sekunden
+
+   #################################################
+# output
+# Hier wird das Ergebnis in der gewünschten Form ausgegeben.
+    print(stunden, "Stunden,", minuten, "Minuten und", sekunden, "Sekunden sind insgesamt", total_sekunden, "Sekunden.") 
+    
+
+###################################################
+
+# Die nächste Zeile nicht verändern
+    return str(stunden) + " Stunden, " + str(minuten) + " Minuten und " + str(sekunden) + " Sekunden sind insgesamt " + str(total_sekunden) + " Sekunden"
+
+# Erwarterter output (Beispiel)
+# Für 7 Stunden, 21 Minuten und 37 Sekunden sollte dein output folgendermaßen aussehen:
+
+#7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden.
+
+# Die nächsten zwei Zeilen nicht verändern
+if __name__ == "__main__":
+    sekundenBerechnen()
