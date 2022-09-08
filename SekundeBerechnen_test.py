2f3d5b6fba48d96ae0ff2f11c8539c652258524f
Marco Savoca <109957478+masa-gymmich@users.noreply.github.com>
2022-09-08 21:28:28 +0200

    Update SekundeBerechnen_test.py

--- SekundeBerechnen_test.py
+++ SekundeBerechnen_test.py
@@ -6,4 +6,4 @@ def test_SekundeBerechnen(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: next(inputs))
     result = SekundenBerechnen.sekundenBerechnen()
     print(result)
-    assert result == '7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden.' or result == '7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden'
+    assert result == '7 Stunden, 21 Minuten und 37 Sekunden sind insgesamt 26497 Sekunden.'
